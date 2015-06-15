0d8fe0c600e2fac779d9d1e3030248721a6041fc
dibley1973 <dibley1973@yahoo.co.uk>
2015-06-15 07:27:38 +0100

    20140615 Added Database project with customer and address tables

---
+++ SimpleCQRSExample/07.01.SimpleCqrsDatabase/customer/Tables/Address.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [customer].[Address]
+(
+    [Id]    INT IDENTITY (1, 1)     NOT NULL
+,   [NumberOrName]  VARCHAR(30)     NOT NULL
+,   [StreetName]    VARCHAR(100)    NOT NULL
+,   [District]      VARCHAR(50)         NULL
+,   [TownCity]      VARCHAR(50)     NOT NULL
+,   [CountyState]   VARCHAR(50)     NOT NULL
+,   [Country]       VARCHAR(50)     NOT NULL
+,   [PostalCode]    VARCHAR(15)         NULL
+,   CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED ([Id])
+)
